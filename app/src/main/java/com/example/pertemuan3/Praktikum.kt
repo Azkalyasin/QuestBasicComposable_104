55b2729caa991306a5c0172d767bb6b03de3bc49
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-10-16 16:23:07 +0700

    pembuatan fun TataletakBox yang berfungsi menampilkan teks di tengah layar dengan Box dan Column.

--- app/src/main/java/com/example/pertemuan3/Praktikum.kt
+++ app/src/main/java/com/example/pertemuan3/Praktikum.kt
@@ -1,13 +1,18 @@
 package com.example.pertemuan3
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.core.content.contentValuesOf
 
 @Composable
 fun TataletakColomn (modifier: Modifier) {
@@ -20,16 +25,31 @@ fun TataletakColomn (modifier: Modifier) {
 }
 
 @Composable
-fun TataletakColumn(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceEvenly
+fun TataletakRow(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
     ) {
-        Text("Component 1")
-        Text("Component 2")
-        Text("Component 3")
-        Text("Component 4")
+        Text("Komponen1")
+        Text("Komponen2")
+        Text("Komponen3")
+        Text("Komponen4")
+    }
+}@Composable
+fun TataletakBox(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxHeight()
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "Box 1")
+            Text(text = "Column 1")
+            Text(text = "Row 1")
+            Text(text = "Box 2")
+            Text(text = "Column 2")
+        }
     }
 }
-
 
