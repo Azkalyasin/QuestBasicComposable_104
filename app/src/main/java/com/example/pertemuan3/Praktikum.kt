7499af419910d33de1e2385af3e7b9df118baa18
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-10-16 15:59:49 +0700

    pembuatan fun TataletakColumn dengan 4 text dalam bentuk Column

--- app/src/main/java/com/example/pertemuan3/Praktikum.kt
+++ app/src/main/java/com/example/pertemuan3/Praktikum.kt
@@ -1,4 +1,19 @@
 package com.example.pertemuan3
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
+@Composable
+fun TataletakColomn (modifier: Modifier) {
+    Column(modifier = Modifier.padding(start = 20.dp, top = 20.dp, end = 20.dp)){
+        Text("component1")
+        Text("component2")
+        Text("component3")
+        Text("component4")
+    }
+}
 
